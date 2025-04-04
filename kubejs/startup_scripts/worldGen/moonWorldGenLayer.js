de886e78be77db71868f2fe6af4d6f17dbbf65ed
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-04-04 20:39:52 +0200

    Added sheldonite moon vein

--- kubejs/startup_scripts/worldGen/moonWorldGenLayer.js
+++ kubejs/startup_scripts/worldGen/moonWorldGenLayer.js
@@ -1,5 +1,5 @@
-// GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
-//     event.create('moon_stone')
-//         .targets('ad_astra:moon_stone')
-//         .dimensions('ad_astra:moon') 
-// })
+GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
+    event.create('moon_stone')
+        .targets('gcyr:moon_stone')
+        .dimensions('gcyr:luna') 
+})
