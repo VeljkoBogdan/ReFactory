e93b13942c30c57d87671e2cca07e4449ed18834
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-04-05 12:28:40 +0200

    Removed Monazite vein from the nether, added it to the moon

--- kubejs/server_scripts/worldGen/oreRemoving.js
+++ kubejs/server_scripts/worldGen/oreRemoving.js
@@ -1,4 +1,5 @@
 GTCEuServerEvents.oreVeins(event => {
     event.remove("gtceu:bauxite_vein_end")
     event.remove("gtceu:sheldonite_vein")
+    event.remove("gtceu:monazite_vein")
 })
--- kubejs/server_scripts/worldGen/veins/moonVeins.js
+++ kubejs/server_scripts/worldGen/veins/moonVeins.js
@@ -2,11 +2,11 @@ GTCEuServerEvents.oreVeins(event => {
     // Moon Sheldonite + Nickel + Magnesite
     event.add('kubejs:sheldonite_vein_moon', vein => {
         vein.layer('moon_rock')
-        vein.weight(20)
+        vein.weight(30)
         vein.clusterSize(85)
         vein.density(0.35)
         vein.discardChanceOnAirExposure(0)
-        vein.heightRangeUniform(16, 128)
+        vein.heightRangeUniform(32, 96)
         vein.layeredVeinGenerator(generator => generator
             .buildLayerPattern(pattern => pattern
                 .layer(l => l.weight(3).mat(GTMaterials.Cooperite).size(2, 4))
@@ -17,6 +17,28 @@ GTCEuServerEvents.oreVeins(event => {
         .surfaceIndicatorGenerator(indicator => indicator
             .surfaceRock(GTMaterials.Cooperite)
             .density(0.2)
+            .radius(4)
+        )
+    })    
+
+    // Moon Neodymium + Monazite + Bastnasite
+    event.add('kubejs:neodymium_vein_moon', vein => {
+        vein.layer('moon_rock')
+        vein.weight(30)
+        vein.clusterSize(85)
+        vein.density(0.35)
+        vein.discardChanceOnAirExposure(0)
+        vein.heightRangeUniform(-50, 64)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.Neodymium).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Monazite).size(1, 3))
+                .layer(l => l.weight(2).mat(GTMaterials.Bastnasite).size(1, 3))
+            )
+        )
+        .surfaceIndicatorGenerator(indicator => indicator
+            .surfaceRock(GTMaterials.Neodymium)
+            .density(0.15)
             .radius(4)
         )
     })
