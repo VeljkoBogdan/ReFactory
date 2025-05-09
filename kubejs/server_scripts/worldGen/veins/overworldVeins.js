281e3202750803fa80b22a071140b5a611ce9ed8
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-05-09 13:00:46 +0200

    Removed all ore indicators

--- kubejs/server_scripts/worldGen/veins/overworldVeins.js
+++ kubejs/server_scripts/worldGen/veins/overworldVeins.js
@@ -1,4 +1,14 @@
 GTCEuServerEvents.oreVeins(event => {
+    // Remove all ore indicators
+    event.modifyAll((veinId, vein) => {
+        vein.surfaceIndicatorGenerator(indicator => indicator
+            .block(Block.getBlock("minecraft:air"))
+            .placement("above")
+            .density(0.4)
+            .radius(5)
+        )
+    })
+
     // Removes Bauxite from Mica Vein forcing the player to go to the Aether to get Rutile
     event.modify("gtceu:mica_vein", vein => {
         vein.layeredVeinGenerator(generator => generator
