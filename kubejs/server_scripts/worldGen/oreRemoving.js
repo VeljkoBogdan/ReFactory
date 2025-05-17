72dd8bd012d1713a7bbffbb10f22a61e2c3d1522
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-05-17 15:49:07 +0200

    Removed Naquadah vein from the end

--- kubejs/server_scripts/worldGen/oreRemoving.js
+++ kubejs/server_scripts/worldGen/oreRemoving.js
@@ -2,4 +2,5 @@ GTCEuServerEvents.oreVeins(event => {
     event.remove("gtceu:bauxite_vein_end")
     event.remove("gtceu:sheldonite_vein")
     event.remove("gtceu:monazite_vein")
+    event.remove("gtceu:naquadah_vein")
 })
