11db8f4d267f7f536fa1f54ba50b116a19db36d9
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-05-30 14:21:52 +0200

    Added Ostrum vein to Mars

--- kubejs/server_scripts/worldGen/veins/marsVeins.js
+++ kubejs/server_scripts/worldGen/veins/marsVeins.js
@@ -1,23 +1,19 @@
-// GTCEuServerEvents.oreVeins(event => {
-//     // event.add('kubejs:aluminium', vein => {
-//     //     vein.layer('mars_rock')
-//     //     vein.weight(20)
-//     //     vein.clusterSize(85)
-//     //     vein.density(0.35)
-//     //     vein.discardChanceOnAirExposure(0)
-//     //     vein.heightRangeUniform(16, 128)
-//     //     vein.layeredVeinGenerator(generator => generator
-//     //         .buildLayerPattern(pattern => pattern
-//     //             .layer(l => l.weight(3).mat(GTMaterials.Aluminium).size(2, 4))
-//     //             .layer(l => l.weight(2).mat(GTMaterials.Bauxite).size(1, 3))
-//     //             .layer(l => l.weight(2).mat(GTMaterials.Ilmenite).size(1, 3))
-//     //         )
-//     //     )
-//     //     .surfaceIndicatorGenerator(indicator => indicator
-//     //         .surfaceRock(GTMaterials.Aluminium)
-//     //         .density(0.2)
-//     //         .radius(4)
-//     //     )
-//     // })
+GTCEuServerEvents.oreVeins(event => {
+    event.add('ostrum_vein', vein => {
+        vein.layer('mars_rock')
+        vein.weight(15)
+        vein.clusterSize(85)
+        vein.density(0.35)
+        vein.discardChanceOnAirExposure(0)
+        vein.heightRangeUniform(16, 128)
+        vein.layeredVeinGenerator(generator => generator
+            .buildLayerPattern(pattern => pattern
+                .layer(l => l.weight(3).mat(GTMaterials.get('ostrum')).size(2, 4))
+                .layer(l => l.weight(2).mat(GTMaterials.Lead).size(1, 3))
+                .layer(l => l.weight(2).mat(GTMaterials.Lithium).size(1, 3))
+            )
+        )
+    })
 
-// })
+    
+})
