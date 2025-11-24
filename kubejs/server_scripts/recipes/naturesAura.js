eb6b75a799472e755064d5e875aae7a12c3ee897
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2025-11-24 16:45:21 +0100

    Slightly Gregified natures aura

--- kubejs/server_scripts/recipes/naturesAura.js
+++ kubejs/server_scripts/recipes/naturesAura.js
@@ -41,4 +41,56 @@ ServerEvents.recipes(event => {
             goldPowder
         ]
     )
+
+    // Wooden stand
+    event.remove({id: 'naturesaura:wood_stand'})
+    event.shaped(
+        'naturesaura:wood_stand',
+        [
+            'ABA',
+            'CDE',
+            '   '
+        ],
+        {
+            A: goldPowder,
+            B: 'gtceu:gold_plate',
+            C: '#minecraft:axes',
+            D: '#minecraft:logs',
+            E: '#forge:tools/files'
+        }
+    )
+
+    // Offering table
+    event.remove({id: 'naturesaura:offering_table'})
+    event.shaped(
+        'naturesaura:offering_table',
+        [
+            'ABA',
+            'CDE',
+            'DDD'
+        ],
+        {
+            A: 'minecraft:stone_slab',
+            B: 'gtceu:infused_iron_plate',
+            C: 'naturesaura:token_fear',
+            D: '#minecraft:logs',
+            E: 'naturesaura:token_sorrow'
+        }
+    )
+
+    // Spirit of Calling
+    event.remove({id: 'naturesaura:calling_spirit'})
+    event.shaped(
+        '4x naturesaura:calling_spirit',
+        [
+            'ABA',
+            'BCB',
+            'ABA'
+        ],
+        {
+            A: 'gtceu:infused_iron_nugget',
+            B: 'minecraft:gold_nugget',
+            C: goldPowder
+        }
+    )
 })
