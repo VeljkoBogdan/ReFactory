72231163f8b215c2d15b6e8e4825f05a5c8495c9
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2026-02-07 12:42:07 +0100

    Added recipes to steam foundry and separator controllers

--- kubejs/server_scripts/recipes/gtceu/multiblocks.js
+++ kubejs/server_scripts/recipes/gtceu/multiblocks.js
@@ -13,4 +13,48 @@ ServerEvents.recipes(event => {
             F: 'ironfurnaces:iron_furnace'
         }
     )
+
+    event.shaped(
+        'refactorycore:steam_foundry',
+        [
+            'ABA',
+            'BCB',
+            'FFF'
+        ],
+        {
+            A: 'gtceu:bronze_gear',
+            B: 'gtceu:steam_machine_casing',
+            C: 'gtceu:lp_steam_alloy_smelter',
+            F: 'furnace'
+        }
+    )
+
+    event.shaped(
+        'refactorycore:steam_foundry',
+        [
+            'ABA',
+            'BCB',
+            'FFF'
+        ],
+        {
+            A: 'gtceu:bronze_gear',
+            B: 'gtceu:steam_machine_casing',
+            C: 'gtceu:hp_steam_alloy_smelter',
+            F: 'furnace'
+        }
+    )
+
+    event.shaped(
+        'refactorycore:steam_separator',
+        [
+            'ABA',
+            'BCB',
+            'ABA'
+        ],
+        {
+            A: 'gtceu:steam_machine_casing',
+            B: 'gtceu:bronze_rotor',
+            C: 'gtceu:bronze_machine_casing'
+        }
+    )
 })
