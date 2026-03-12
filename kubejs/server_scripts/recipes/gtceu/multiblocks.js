fb37dd1f9b79201e8fb59166720420e33db0ee95
VeljkoBogdan <vtsveljkobogdan@gmail.com>
2026-03-12 23:17:55 +0100

    basic fluid rig added

--- kubejs/server_scripts/recipes/gtceu/multiblocks.js
+++ kubejs/server_scripts/recipes/gtceu/multiblocks.js
@@ -57,4 +57,49 @@ ServerEvents.recipes(event => {
             C: 'gtceu:bronze_machine_casing'
         }
     )
+
+    event.recipes.gtceu.assembler('basic_fluid_rig')
+        .itemInputs(
+            'gtceu:lv_machine_hull',
+            '4x gtceu:steel_frame',
+            '4x #gtceu:circuits/lv',
+            '4x gtceu:lv_electric_motor',
+            '4x gtceu:lv_electric_pump',
+            '4x gtceu:cobalt_brass_gear'
+        )
+        .circuit(2)
+        .itemOutputs('refactorycore:lv_basic_fluid_drilling_rig')
+        .duration(20*20)
+        .EUt(GTValues.VA[GTValues.LV])
+
+    event.shaped(
+        'refactorycore:aura_altar',
+        [
+            'ABA',
+            'CDC',
+            'EEE'
+        ],
+        {
+            A: '#gtceu:circuits/mv',
+            B: 'gtceu:lv_sensor',
+            C: 'naturesaura:infused_iron_block',
+            D: 'naturesaura:nature_altar',
+            E: 'naturesaura:infused_brick'
+        }
+    )
+
+    event.shaped(
+        'refactorycore:cooling_tower',
+        [
+            'ABA',
+            'CDC',
+            'ABA'
+        ],
+        {
+            A: 'gtceu:stainless_steel_rotor',
+            B: 'ad_astra:vent',
+            C: '#gtceu:circuits/hv',
+            D: 'gtceu:ev_machine_hull'
+        }
+    )
 })
